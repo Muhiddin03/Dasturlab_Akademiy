6ddc2be6bb6ca4f6db4677d159e8e3c21c1b8a6c
Muhiddin <karimjonovmuhiddin13@gmail.com>
2026-05-31 13:21:21 +0500

    Update Supabase configuration with new URL and key

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 //   config.js
 // =============================================
 window.SUPABASE_CONFIG = {
-  url: 'https://XXXXXXXX.supabase.co',   // <-- Supabase URL
-  key: 'eyXXXXXXXXXXXXXXXXXXXXXXXXXX'  // <-- Supabase anon key
+  url: 'https://zxaplrdoerblkoeoxhzd.supabase.co/rest/v1',   // <-- Supabase URL
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4YXBscmRvZXJibGtvZW94aHpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMjE1MjQsImV4cCI6MjA5Mzc5NzUyNH0.bG_-MZn97FOHbhi6G9z66V2tDkq6wL-wFcnGQmg0xnY'  // <-- Supabase anon key
 };
