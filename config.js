b9adc38781cdae996f5e87549688abdd711261d4
Muhiddin <karimjonovmuhiddin13@gmail.com>
2026-07-05 20:17:12 +0500

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 //   config.js
 // =============================================
 window.SUPABASE_CONFIG = {
-  url: 'https://zxaplrdoerblkoeoxhzd.supabase.co/rest/v1',   // <-- Supabase URL
+  url: 'https://zxaplrdoerblkoeoxhzd.supabase.co',   // <-- Supabase URL
   key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4YXBscmRvZXJibGtvZW94aHpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMjE1MjQsImV4cCI6MjA5Mzc5NzUyNH0.bG_-MZn97FOHbhi6G9z66V2tDkq6wL-wFcnGQmg0xnY'  // <-- Supabase anon key
 };
